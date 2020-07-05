b851a224c06931294489d79d36f802e9315921f9
irvin373 <irvin373@gmail.com>
2020-07-05 13:02:39 -0400

    plant details ui

--- app/src/main/java/com/example/tukuypacandroid/PlantDetail.kt
+++ app/src/main/java/com/example/tukuypacandroid/PlantDetail.kt
@@ -1,17 +1,21 @@
 package com.example.tukuypacandroid
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.navArgs
 import com.example.tukuypacandroid.data.RoomSingleton
 import com.example.tukuypacandroid.data.model.PlantWithGroup
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
+import java.io.IOException
 
 class PlantDetail : Fragment() {
 
@@ -23,18 +27,34 @@ class PlantDetail : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.plant_detail_fragment, container, false)
         val safeArgs: PlantDetailArgs by navArgs()
-        val plantId = safeArgs.plantIdNumber
+        val plantId = safeArgs.plantIdNumber + 1
         this.getDetailData(root, container?.context as Context, plantId)
         return root
     }
 
     private fun getDetailData(root: View, context: Context, plantId: Int) {
-        val textView: TextView = root.findViewById(R.id.titleName)
+        val textViewName: TextView = root.findViewById(R.id.titleName)
+        val textViewGroup: TextView = root.findViewById(R.id.groupName)
+        val textViewDescription: TextView = root.findViewById(R.id.description)
+        val imageView: ImageView = root.findViewById(R.id.image)
         GlobalScope.async {
             val db = RoomSingleton.getInstance(context)
             plant = db.plantDao().getSinglePlants(plantId)
             println("here ${plant?.group?.name} ${plant?.plant?.name}")
-            textView.text = plant?.group?.name ?: "waiting"
+            textViewName.text = plant?.plant?.name ?: "waiting"
+            textViewGroup.text = plant?.group?.name ?: "waiting"
+            textViewDescription.text = plant?.plant?.description ?: "waiting"
+            val assetBitmap: Bitmap? = getBitmapFromAssets(plant?.plant?.imgDir as String, context)
+            imageView.setImageBitmap(assetBitmap)
+        }
+    }
+
+    private fun getBitmapFromAssets(fileName: String, context: Context): Bitmap? {
+        return try {
+            BitmapFactory.decodeStream(context.assets.open(fileName))
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
         }
     }
 }

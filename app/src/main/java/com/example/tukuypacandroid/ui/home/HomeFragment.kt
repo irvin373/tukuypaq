8edd817472c202a4667577c5751238cdfd248b61
irvin373 <irvin373@gmail.com>
2020-07-10 23:17:06 -0400

    fix ui style

--- app/src/main/java/com/example/tukuypacandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tukuypacandroid/ui/home/HomeFragment.kt
@@ -1,14 +1,16 @@
 package com.example.tukuypacandroid.ui.home
 
 import android.content.Context
-import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.tukuypacandroid.R
 import com.example.tukuypacandroid.data.RoomSingleton
+import com.example.tukuypacandroid.data.model.PlantWithGroup
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -16,7 +18,9 @@ import kotlinx.coroutines.async
 
 class HomeFragment : Fragment() {
 
+    private lateinit var plantViewModel: HomeViewModel
     private lateinit var adapter: PlantsAdapter
+    private lateinit var plantsList: List<PlantWithGroup>
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -24,7 +28,14 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        plantsList = emptyList()
+        plantViewModel =
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         this.getPlants(container?.context as Context)
+        plantViewModel.plants.observe(viewLifecycleOwner, Observer {
+            adapter = PlantsAdapter(plantsList)
+            plantlistView.adapter = adapter
+        })
         return root
     }
 
@@ -33,19 +44,10 @@ class HomeFragment : Fragment() {
             // do work here ...
             val db = RoomSingleton.getInstance(container);
             val plants = db.plantDao().getPlantsWithGroup()
-            adapter = PlantsAdapter(plants)
+            plantsList = plants
+            adapter = PlantsAdapter(plantsList)
             plantlistView.adapter = adapter
         }
     }
 
-}
-
-class doAsync(val handler: () -> Unit) : AsyncTask<Void, Void, Void>() {
-    init {
-        execute()
-    }
-    override fun doInBackground(vararg params: Void?): Void? {
-        handler()
-        return null
-    }
 }
--- app/src/main/java/com/example/tukuypacandroid/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/tukuypacandroid/ui/home/HomeViewModel.kt
@@ -3,10 +3,11 @@ package com.example.tukuypacandroid.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tukuypacandroid.data.model.PlantWithGroup
 
 class HomeViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _plants = MutableLiveData<List<PlantWithGroup>>().apply {
+        value = emptyList()
     }
-    val text: LiveData<String> = _text
+    val plants: LiveData<List<PlantWithGroup>> = _plants
 }
